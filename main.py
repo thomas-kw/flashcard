1ce3289d6a9ecad29caceafa39f6bc6a83cfc9b0
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-08-08 20:45:28 +0900

    right answer button

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = "Arial", 40, "italic"
 WORD_FONT = "Arial", 60, "bold"
+
+
+# -----------------------Word Picker----------------#
+def pick_word():
+    canvas.delete("label")
+    random_word = random.choice(list(file_df["French"]))
+    canvas.create_text(400, 263, text=random_word, font=WORD_FONT, tags="label")
+
+# -----------------#
+
 
 window = Tk()
 window.title("Flashcard")
@@ -12,14 +24,18 @@ card_front_img = PhotoImage(file="images/card_front.png")
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
 
+file = pd.read_csv("data/french_words.csv")
+file_df = pd.DataFrame(file)
+print(file_df)
+
 canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=0, columnspan=2)
-canvas.create_text(400, 150, text="French", font=(LANGUAGE_FONT))
-canvas.create_text(400, 263, text="trouve", font=(WORD_FONT))
+canvas.create_text(400, 150, text="French", font=LANGUAGE_FONT)
+canvas.create_text(400, 263, text=pick_word(), font=WORD_FONT)
 
-known_button = Button(image=right_img, highlightthickness=0)
+known_button = Button(image=right_img, highlightthickness=0, command=pick_word)
 known_button.grid(row=1, column=0)
 
 unknown_button = Button(image=wrong_img, highlightthickness=0)
