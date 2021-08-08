7a392a54951bdd3b3596f38831dc960ea8a4018c
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-08-08 15:52:35 +0900

    added buttons

--- main.py
+++ main.py
@@ -19,12 +19,10 @@ canvas.grid(row=0, column=0, columnspan=2)
 canvas.create_text(400, 150, text="French", font=(LANGUAGE_FONT))
 canvas.create_text(400, 263, text="trouve", font=(WORD_FONT))
 
-canvas = Canvas(width=100, height=100, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(50, 50, image=right_img)
-canvas.grid(row=1, column=0)
+known_button = Button(image=right_img, highlightthickness=0)
+known_button.grid(row=1, column=0)
 
-canvas = Canvas(width=100, height=100, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(50, 50, image=wrong_img)
-canvas.grid(row=1, column=1)
+unknown_button = Button(image=wrong_img, highlightthickness=0)
+unknown_button.grid(row=1, column=1)
 
 window.mainloop()
