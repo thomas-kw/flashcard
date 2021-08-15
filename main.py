eef220d69644bfeb52d609c4df9df24a57d3809c
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-08-15 20:10:59 +0900

    save feature

--- main.py
+++ main.py
@@ -5,31 +5,48 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = "Arial", 40, "italic"
 WORD_FONT = "Arial", 60, "bold"
-
-data = pd.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-
 current_card = {}
+to_learn = {}
 
-# -----------------------Word Picker----------------#
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+# --------Next Card--------#
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    #This is to cancel the previous timer (in case user keeps clicking buttons fast)
+    # This is to cancel the previous timer (in case user keeps clicking buttons fast)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_background, image=card_front_img)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     flip_timer = window.after(3000, func=flip_card)
 
-# -----------------#
+
+# --------Flip Cards---------#
 def flip_card():
     canvas.itemconfig(card_background, image=card_back_img)
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
-#------------------#
+# --------Is Known-----------#
+def is_known():
+    to_learn.remove(current_card)
+    df = pd.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv")
+    next_card()
+
+
+# ----------Window-----------#
+
 window = Tk()
 window.title("Flashcard")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -48,13 +65,12 @@ canvas.grid(row=0, column=0, columnspan=2)
 card_title = canvas.create_text(400, 150, text="Title", font=LANGUAGE_FONT)
 card_word = canvas.create_text(400, 263, text="word", font=WORD_FONT)
 
-known_button = Button(image=right_img, highlightthickness=0, command=next_card)
-known_button.grid(row=1, column=0)
+known_button = Button(image=right_img, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
 unknown_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
-unknown_button.grid(row=1, column=1)
+unknown_button.grid(row=1, column=0)
 
 next_card()
-
 
 window.mainloop()
