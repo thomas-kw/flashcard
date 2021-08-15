de56c556658716d3952ec0e110306eae3fdc718e
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-08-15 17:29:08 +0900

    fixed buttons

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ WORD_FONT = "Arial", 60, "bold"
 
 
 # -----------------------Word Picker----------------#
-def pick_word():
+def next_card():
     canvas.delete("label")
-    random_word = random.choice(list(file_df["French"]))
-    canvas.create_text(400, 263, text=random_word, font=WORD_FONT, tags="label")
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 # -----------------#
 
@@ -24,21 +25,22 @@ card_front_img = PhotoImage(file="images/card_front.png")
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
 
-file = pd.read_csv("data/french_words.csv")
-file_df = pd.DataFrame(file)
-print(file_df)
+data = pd.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
 
 canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=0, columnspan=2)
-canvas.create_text(400, 150, text="French", font=LANGUAGE_FONT)
-canvas.create_text(400, 263, text=pick_word(), font=WORD_FONT)
+card_title = canvas.create_text(400, 150, text="Title", font=LANGUAGE_FONT)
+card_word = canvas.create_text(400, 263, text="word", font=WORD_FONT)
 
-known_button = Button(image=right_img, highlightthickness=0, command=pick_word)
+known_button = Button(image=right_img, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=0)
 
-unknown_button = Button(image=wrong_img, highlightthickness=0)
+unknown_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
